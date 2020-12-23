cca403ee579420d1dcd7d32364b370c244f804cb
Iain McCoy <iain@mccoy.id.au>
2020-12-23 19:06:18 +1100

    fast enough

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -46,59 +46,23 @@ fn tile_fingerprints(tile : &Vec<Vec<bool>>) -> Vec<(usize, usize, usize, usize)
     vec!(
         (top,           right,        bottom,        left),
         (inv10(top),    inv10(left),  inv10(bottom), inv10(right)), // flip along horizontal axis
-        (inv10(bottom), inv10(right), inv10(top),    inv10(left)),  // flip along vertical axis
-        (top,           left,         bottom,        right)  // flip both
     )
 }
 
-fn explore(grid_size : usize, tiles_by_fingerprints : &HashMap<usize, Vec<(usize, (usize, usize, usize, usize))>>, arrangement : &mut Vec<(usize, (usize, usize, usize, usize))>) {
-    //if arrangement.len() > 7 {
-    //    println!("{} {}", arrangement.len(), arrangement.iter().map(|(t, _)| t.to_string()).collect::<Vec<String>>().join(" "));
-    //}
-    //if arrangement.len() == 8 {
-    //    for (n, prints) in arrangement.iter() {
-    //      println!("  {:?} {:?}", n, &prints);
-    //    }
-    //}
-    if arrangement.len() == grid_size * grid_size {
-        dbg!(grid_size);
-        dbg!(arrangement[0].0, arrangement[grid_size - 1].0, arrangement[arrangement.len() - 1].0, arrangement[arrangement.len() - grid_size - 1].0);
+fn find_lines_from(line_length : usize, tiles_by_fingerprints : &HashMap<usize, Vec<(usize, (usize, usize, usize, usize))>>, line : &mut Vec<(usize, (usize, usize, usize, usize))>) -> Vec<Vec<(usize, (usize, usize, usize, usize))>> {
+    if line.len() == line_length {
+        return vec!(line.clone());
     }
-    if arrangement.len() == 0 {
-        for (number, prints) in tiles_by_fingerprints.values().map(|v| v.iter()).flatten() {
-            arrangement.push((*number, *prints));
-            explore(grid_size, tiles_by_fingerprints, arrangement);
-            arrangement.pop();
+    let end_of_line = line[line.len() - 1];
+    let mut result : Vec<Vec<(usize, (usize, usize, usize, usize))>> = Vec::new();
+    for (next_id, next_prints) in tiles_by_fingerprints.get(&inv10(end_of_line.1.2)).unwrap_or(&vec!()) {
+        if line.iter().find(|(line_id, _)| line_id == next_id).is_none() {
+            line.push((*next_id, *next_prints));
+            result.extend(find_lines_from(line_length, tiles_by_fingerprints, line));
+            line.pop();
         }
-    } else if arrangement.len() < grid_size {
-        for (number, prints) in tiles_by_fingerprints.get(&inv10(arrangement[arrangement.len()-1].1.2)).unwrap_or(&vec!()) {
-            if arrangement.iter().find(|(n, _)| n == number).is_none() {
-                arrangement.push((*number, *prints));
-                explore(grid_size, tiles_by_fingerprints, arrangement);
-                arrangement.pop();
-            }
-        }
-    } else if arrangement.len() % grid_size == 0 {
-        for (number, prints) in tiles_by_fingerprints.get(&inv10(arrangement[arrangement.len() - grid_size].1.3)).unwrap_or(&vec!()) {
-            if arrangement.iter().find(|(n, _)| n == number).is_none() {
-                arrangement.push((*number, rot1(*prints)));
-                explore(grid_size, tiles_by_fingerprints, arrangement);
-                arrangement.pop();
-            }
-        }
-    } else {
-        for (number, prints) in tiles_by_fingerprints.get(&inv10(arrangement[arrangement.len()-1].1.2)).unwrap_or(&vec!()) {
-            //if arrangement.len() == 8 {
-            //    dbg!(number, prints, (inv10(prints.0), inv10(prints.1), inv10(prints.2), inv10(prints.3)), arrangement[arrangement.len() - grid_size].1.3, prints.1);
-            //}
-            if arrangement.iter().find(|(n, _)| n == number).is_none() && inv10(arrangement[arrangement.len() - grid_size].1.3) == prints.1 {
-                arrangement.push((*number, *prints));
-                explore(grid_size, tiles_by_fingerprints, arrangement);
-                arrangement.pop();
-            }
-        }
-
     }
+    return result;
 }
 
 fn main() {
@@ -106,6 +70,7 @@ fn main() {
     let mut current_tile : Vec<Vec<bool>> = Vec::new();
     let mut tiles : Vec<(usize, Vec<Vec<bool>>)> = Vec::new();
     let mut tiles_by_fingerprints : HashMap<usize, Vec<(usize, (usize, usize, usize, usize))>> = HashMap::new();
+    let mut fingerprints_by_tile : HashMap<usize, Vec<(usize, usize, usize, usize)>> = HashMap::new();
     for wrapped_line in io::stdin().lock().lines() {
         let line = wrapped_line.unwrap();
         if line.len() == 0 {
@@ -127,10 +92,70 @@ fn main() {
                 tiles_by_fingerprints.entry(rotation.0)
                     .or_insert(Vec::new())
                     .push((*tile_number, rotation));
+                fingerprints_by_tile.entry(*tile_number)
+                    .or_insert(Vec::new())
+                    .push(rotation);
             }
         }
     }
 
-    let mut arrangement : Vec<(usize, (usize, usize, usize, usize))> = Vec::new();
-    explore((tiles.len() as f32).sqrt() as usize, &tiles_by_fingerprints, &mut arrangement);
+    let grid_size = (tiles.len() as f32).sqrt() as usize;
+    let mut lines : Vec<Vec<(usize, (usize, usize, usize, usize))>> = Vec::new();
+    
+    for (tile_number, prints) in fingerprints_by_tile {
+        for print in prints {
+            lines.extend(find_lines_from(grid_size, &tiles_by_fingerprints, &mut vec!((tile_number, print))));
+        }
+    }
+    dbg!(lines.iter().map(|line| line.iter().map(|t| t.0.to_string()).collect::<Vec<String>>().join(" ")).collect::<Vec<String>>());
+
+    let mut lines_by_starting_cell : HashMap<usize, Vec<&Vec<(usize, (usize, usize, usize, usize))>>> = HashMap::new();
+    for line in &lines {
+        println!("{}", line[0].0);
+        lines_by_starting_cell.entry(line[0].0)
+            .or_insert(Vec::new())
+            .push(line);
+    }
+
+    for (starting_cell, lines) in &lines_by_starting_cell {
+        for (line_number, line) in lines.iter().enumerate() {
+            for other_line in lines.iter() {
+                if line.get(1..).unwrap().iter().find(|cell| other_line.iter().find(|other_cell| other_cell.0 == cell.0).is_some()).is_some() {
+                    continue;
+                }
+                let mut cells : Vec<usize> = Vec::new();
+                cells.extend(other_line.iter().map(|cell| cell.0));
+                match fill_out_grid(&lines_by_starting_cell, line, other_line, &cells, 1) {
+                    Some(last_row) => {
+                        dbg!(other_line[0], other_line[other_line.len() - 1], last_row[0], last_row[last_row.len() - 1]);
+                        dbg!(other_line[0].0 * other_line[other_line.len() - 1].0 * last_row[0].0 * last_row[last_row.len() - 1].0);
+                    },
+                    None => { /* do nothing */ }
+                }
+            }
+        }
+    }
+}
+
+fn fill_out_grid(lines_by_starting_cell : &HashMap<usize, Vec<&Vec<(usize, (usize, usize, usize, usize))>>>, column : &Vec<(usize, (usize, usize, usize, usize))>, row : &Vec<(usize, (usize, usize, usize, usize))>, cells : &Vec<usize>, row_number : usize) -> Option<Vec<(usize, (usize, usize, usize, usize))>> {
+    if row_number == 12 {
+        return Some(row.clone());
+    }
+    'candidates: for candidate in lines_by_starting_cell.get(&column[row_number].0).unwrap() {
+        if candidate.iter().find(|line_elem| cells.contains(&line_elem.0)).is_some() {
+            continue;
+        }
+        for ((_, (_, _, _, prev_bottom)), (_, (_, top, _, _))) in row.iter().zip(candidate.iter()) {
+            if inv10(*prev_bottom) != *top {
+                continue 'candidates;
+            }
+        }
+        let mut new_cells = cells.clone();
+        new_cells.extend(candidate.iter().map(|cell| cell.0));
+        match fill_out_grid(lines_by_starting_cell, column, &candidate, &new_cells, row_number + 1) {
+            Some(result) => return Some(result),
+            _ => { /* do nothing */ }
+        }
+    }
+    return None;
 }
